408f721385c1461a92fe8038afc5bcfec2e4bdc1
Christian Hoene <christian.hoene@symonics.com>
2017-02-22 16:16:40 +0100

    Added a tester sofa file for better test cases

---
+++ tests/tester.m
@@ -0,0 +1,31 @@
+for n=1:size(hrtf.SourcePosition,1)
+    a = round(hrtf.SourcePosition(n,1) / 2);
+    
+    for u=1:size(hrtf.Data.IR,3) 
+        if u == a
+            hrtf.Data.IR(n,1,u) = 1;
+        elseif (u > a & u <= a+8)
+            hrtf.Data.IR(n,1,u) = -bitget(a,u-a);
+        else
+            hrtf.Data.IR(n,1,u) = 0;
+        end
+    end
+        
+    a = round(hrtf.SourcePosition(n,2) / 2);
+    if a < 0 
+        a = a + 180;
+    end
+ 
+    for u=1:size(hrtf.Data.IR,3) 
+        if u == a
+            hrtf.Data.IR(n,2,u) = 1;
+        elseif (u > a & u <= a+8)
+            hrtf.Data.IR(n,2,u) = -bitget(a,u-a);
+        else
+            hrtf.Data.IR(n,2,u) = 0;
+        end
+    end
+            
+end
+    
+   
