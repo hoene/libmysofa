0571940a31ad4e1bec944dbef834ee14b9f0c39a
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2017-11-20 16:38:47 +0100

    Initial pkg-config support
    Closes: https://github.com/hoene/libmysofa/issues/38

---
+++ libmysofa.pc.cmake
@@ -0,0 +1,12 @@
+Name: @PROJECT_NAME@
+Description: @CPACK_PACKAGE_DESCRIPTION@
+Version: @PROJECT_VERSION@
+Requires: @PKG_CONFIG_REQUIRES@
+prefix=@CMAKE_INSTALL_PREFIX@
+includedir=${prefix}/include
+libdir=${prefix}/lib
+Libs: -L${libdir} -lmysofa
+Cflags: -I${includedir}
+
+#Libs.private:
+#Requires.private:
