f571522bfcee062b1277f6e480a90e4802b8e301
Christian Hoene <christian.hoene@symonics.com>
2019-08-25 13:39:50 +0200

    Replaced malloc to calloc

--- src/hdf/dataobject.c
+++ src/hdf/dataobject.c
@@ -375,7 +375,7 @@ static int readOHDRHeaderMessageDataLayout(struct READER *reader,
 				return errno;
 			if (!data->data) {
 				data->data_len = size;
-				data->data = malloc(size);
+				data->data = calloc(1,size);
 				if (!data->data)
 					return MYSOFA_NO_MEMORY;
 			}
