907d890ce3b794b160f9de45f43777e35fe9f304
Christian Hoene <christian.hoene@symonics.com>
2020-06-09 16:19:49 +0200

    changed?

--- src/hdf/dataobject.c
+++ src/hdf/dataobject.c
@@ -599,37 +599,52 @@ static int readOHDRHeaderMessageGroupInfo(struct READER *reader,
  *
  */
 
+/* type 1
+
+                                                        00  |......G.8.......|
+000010a0  00 00 00 00 00 02 00 08  00 01 00 01 00 73 68 75  |.............shu|
+000010b0  66 66 6c 65 00 08 00 00  00 00 00 00 00 01 00 08  |ffle............|
+000010c0  00 01 00 01 00 64 65 66  6c 61 74 65 00 01 00 00  |.....deflate....|
+000010d0  00 00 00 00 00 08 17 00  01 00 00 03 02 03 01 48  |...............H|
+*/
 static int readOHDRHeaderMessageFilterPipelineV1(struct READER *reader, uint8_t filters) {
   int i, j;
-  uint16_t filter_identification_value, flags, number_client_data_values;
+  uint16_t filter_identification_value, flags, number_client_data_values, namelength;
   uint32_t client_data;
   uint64_t maximum_compact_value, minimum_dense_value, number_of_entries,
       length_of_entries;
 
+/*
   UNUSED(flags);
   UNUSED(client_data);
   UNUSED(maximum_compact_value);
   UNUSED(minimum_dense_value);
   UNUSED(number_of_entries);
   UNUSED(length_of_entries);
+*/
 
   for (i = 0; i < filters; i++) {
     filter_identification_value = (uint16_t)readValue(reader, 2);
     switch (filter_identification_value) {
-    case 1:
-    case 2:
+    case 0:
       break;
     default:
       // LCOV_EXCL_START
       mylog("object OHDR filter pipeline message contains unsupported filter: "
-            "%d\n",
-            filter_identification_value);
+            "%d %lX\n",
+            filter_identification_value, ftell(reader->fhd)-2);
       return MYSOFA_INVALID_FORMAT;
       // LCOV_EXCL_STOP
     }
-    mylog("  filter %d\n", filter_identification_value);
+    namelength = (uint16_t)readValue(reader, 2);
     flags = (uint16_t)readValue(reader, 2);
     number_client_data_values = (uint16_t)readValue(reader, 2);
+
+    if(namelength>0)
+      fseek(reader->fhd, ((namelength-1)&~7) + 8, SEEK_CUR); // skip name
+
+    mylog("  filter %d namelen %d flags %04X values %d\n", filter_identification_value,namelength,flags,number_client_data_values);
+
     if (number_client_data_values > 0x1000)
       return MYSOFA_UNSUPPORTED_FORMAT; // LCOV_EXCL_LINE
     /* no name here */
