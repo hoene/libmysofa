8dda834bf9851357fdaae7a6ebd76b157bb91c7a
Christian Hoene <christian.hoene@symonics.com>
2021-01-24 18:20:42 +0100

    Fix coverity warning

--- src/hdf/dataobject.c
+++ src/hdf/dataobject.c
@@ -67,11 +67,11 @@ static int readOHDRHeaderMessageDataspace1(struct READER *reader,
     if (i < 4) {
       ds->dimension_size[i] =
           readValue(reader, reader->superblock.size_of_lengths);
-      mylog("   dimension %d %" PRIu64 "\n", i, ds->dimension_size[i]);
-      if (ds->dimension_size[i] > 1000000) {
+      if (ds->dimension_size[i] < 0 || ds->dimension_size[i] > 1000000) {
         mylog("dimension_size is too large\n"); // LCOV_EXCL_LINE
         return MYSOFA_INVALID_FORMAT;           // LCOV_EXCL_LINE
       }
+      mylog("   dimension %d %" PRIu64 "\n", i, ds->dimension_size[i]);
     } else
       readValue(reader, reader->superblock.size_of_lengths);
   }
